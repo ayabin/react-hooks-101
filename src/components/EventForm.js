d1b58aeb75bcc18bfdb0ef1fd45201564977b2c5
ayabin <ayabin.jp@gmail.com>
2021-12-31 11:06:39 +0900

    delete all operation logs

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -5,7 +5,7 @@ import {
   CREATE_EVENT,
   DELETE_ALL_EVENTS,
   ADD_OPERATION_LOG,
-  DELETE_OPERATION_LOGS
+  DELETE_ALL_OPERATION_LOGS
 } from '../actions';
 import {timeCurrentIso8601} from '../utils';
 
@@ -45,6 +45,16 @@ const EventForm=()=>{
 
   const unCreatable=title==='' || body==='' 
   const unRemovable=state.events.length===0;
+
+  const deleteAllOperationLogs=e=>{
+    e.preventDefault();
+    const result=window.confirm('全ての操作ログを本当に削除しても良いですか？');
+    if(result){
+      dispatch({
+        type:DELETE_ALL_OPERATION_LOGS
+      });
+    }
+  }
   return (
     <>
       <h4>イベント作成フォーム</h4>
@@ -59,7 +69,7 @@ const EventForm=()=>{
         </div>
         <button className='btn btn-primary' onClick={addEvent} disabled={unCreatable}>イベントを作成する</button>
         <button className='btn btn-danger' onClick={deleteAllEvents} disabled={unRemovable}>全てのイベントを削除する</button>
-        {/* <button className='btn btn-danger'>全てのイベントを削除する</button> */}
+        <button className='btn btn-danger' onClick={deleteAllOperationLogs} disabled={state.operationLogs.length===0}>全ての操作ログを削除する</button>
       </form>
     </>
   );
